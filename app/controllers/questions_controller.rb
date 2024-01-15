c70fc0174e85d903a0e40565c9ea89c0c0ae6c0d
juan bernal <mesieou@gmail.com>
2024-01-15 12:29:51 +1100

    styling

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,20 +3,18 @@ class QuestionsController < ApplicationController
     @question = 'Ask!'
   end
 
-  def couch_reply(answer)
-    if @answer == 'i am going to work'
-      @couch_answer = 'Great!'
-      @couch_answer
-    elsif @answer[-1] == ('?')
-      @couch_answer = 'Silly question, get dressed and go to work!'
-      @couch_answer
-    else
-      @couch_answer = "I don't care, get dressed and go to work!"
-    end
+  def couch_reply
+    @couch_answer = if @answer == 'i am going to work'
+                      'Great!'
+                    elsif @answer[-1] == ('?')
+                      'Silly question, get dressed and go to work!'
+                    else
+                      "I don't care, get dressed and go to work!"
+                    end
   end
 
   def display
     @answer = params[:answer].downcase
-    couch_reply(@answer)
+    couch_reply
   end
 end
